b82659f1a08657f6e4b6fa43bbd584d6ce4ff02b
hazem ashraf <hazem.ashraf126@gmail.com>
2023-12-27 18:50:42 +0200

    add build files for owner app

---
+++ apps/owner/build/app/generated/source/buildConfig/debug/com/spotx/owner/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.spotx.owner;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.spotx.owner";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 34;
+  public static final String VERSION_NAME = "1.0.34";
+}
